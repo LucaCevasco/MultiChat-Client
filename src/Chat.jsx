0738349dc06b42ff79319b6f9e7c194acad74dcb
Luca Cevasco <thelucazip@gmail.com>
2020-09-21 20:32:44 -0300

    [FEATURE] ESLINT setup && graphql connection

---
+++ src/Chat.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+import { ApolloClient, 
+	InMemoryCache,
+	ApolloProvider, 
+	useQuery, 
+	gql 
+} from '@apollo/client';
+import {
+	Container,
+} from 'shards-react';
+
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/',
+	cache: new InMemoryCache()
+});
+
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+      user
+    }
+  }
+`;
+
+// eslint-disable-next-line no-unused-vars
+const Messages = ({ user }) => {
+	const { data } = useQuery(GET_MESSAGES);
+	if (!data) return null;
+
+	return JSON.stringify(data);
+};
+
+const Chat = () => {
+	return (
+		<Container>
+			<Messages user="Jack" />
+		</Container>
+	);
+};
+
+export default () => (
+	<ApolloProvider client={client}>
+		<Chat />
+	</ApolloProvider>
+);
